1fd70eed6fa82e474549c572382956c5077e3136
=bibin thomas <bibinthomas951@gmail.com>
2022-07-01 01:49:06 +0530

    first commit

--- project/views.py
+++ project/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render
-from flask import render_template ,Blueprint
-
+from flask_sqlalchemy import SQLAlchemy
+from flask import render_template ,Blueprint,url_for,request
 
 views = Blueprint('views', __name__)
+
+
+
+db = SQLAlchemy(app)
+class UserData(db.model):
+    
+
 
 @views.route("/")
 def create_account():
     return render_template("create_Acc.html")
 
-@views.route("/login")
+@views.route("/login",methods=["POST"])
 def login():
     return render_template("login.html")
 
