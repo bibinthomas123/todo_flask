421a91a7f1604a0e856c03deee85c9a2e68457ea
=bibin thomas <bibinthomas951@gmail.com>
2022-07-04 19:38:28 +0530

    Added small designs and re-solved the flash error

--- project/auth.py
+++ project/auth.py
@@ -5,6 +5,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 from flask_login import login_user, login_required, logout_user, current_user
 from sqlalchemy.exc import IntegrityError
+import re 
 
 
 auth = Blueprint('auth', __name__)
@@ -19,15 +20,15 @@ def login():
         user = User.query.filter_by(username=username).first() 
         if user:
             if check_password_hash(user.password, password):
-                # flash('Logged in successfully!', category='success')
+                flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
                 print("passed checking")
                 return redirect(url_for('views.home'))
             else:
-                # flash('Incorrect password, try again.', category='error')
+                flash('Incorrect password, try again.', category='error')
                 print("error password")
         else:
-            # flash('Username does not exist.', category='error')
+            flash('Username does not exist.', category='error')
             print("error")
 
     return render_template("login.html", user=current_user)
@@ -42,28 +43,29 @@ def logout():
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+    pattern ="(?=.*[a-z])(?=.*[0-9])(?=.*[A-Z])(?=.*[!?@])[a-zA-Z0-9!?@]{8,}" 
     if request.method == "POST":
         firstname = request.form.get("new_full_name")
         username = request.form.get("new_user_name")
         password = request.form.get("new_password")
 
         user = User.query.filter_by(username=username).first()
-        if user:
-            flash('Email already exists.', category='error')
+        # if user:
+        #     flash('username already exists.', category='error')
         if len(firstname) < 2:
             flash('First name must be greater than 1 character.', category='error')
-        elif len(password) < 7:
-            flash('Password must be at least 7 characters.', category='error')
-        else:
+        if (re.search(pattern,password)):
             try:
                 new_user = User(username=username, password=generate_password_hash(password, method='sha256'), first_name=firstname)
                 db.session.add(new_user)
                 db.session.commit()
                 print("added user")
                 login_user(new_user, remember=True)
-                # flash('Account created!', category='success')
+                flash('Account created!', category='success')
                 return redirect(url_for('views.home'))
             except IntegrityError:
                 db.session.rollback()
-                return "<h1>already exists</h1>"
+                return f'<h1> the username "{user.username}" is already taken</h1>'
+        else:
+            flash('Weak Password !! Try again', category='error')
     return render_template("create_Acc.html")
